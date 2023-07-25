fceb93393acadd12a18a620a24f09cdd36a2a36d
am <am2022a2@gmail.com>
2023-07-25 12:49:34 +0330

    set_pass_len() created in gen.cpp

--- src/gen.cpp
+++ src/gen.cpp
@@ -7,3 +7,12 @@ gen::gen(){
 gen::gen(string u_ch){
     this->ch = u_ch;
 }
+
+void gen::set_pass_len(int u_pass_len){
+    if(u_pass_len > 0){
+        this->pass_len = u_pass_len;
+    }
+    else{
+        return 0;
+    }
+}
